7e0af868de7d53065979bbe625b09adddddca582
Mihai Safta <mihaisafta@endava.com>
2020-04-10 14:57:48 +0300

    added a test

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstClass {
+        private WebDriver driver;
+
+        public void setUp(){
+            System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+        }
+
+
+
     public static void main(String[] args) {
-        System.out.println("hellou");
+        FirstClass test = new FirstClass();
+        test.setUp();
     }
 }
